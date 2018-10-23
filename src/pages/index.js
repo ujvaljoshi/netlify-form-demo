4cd2daf3d283369394e203b4e7d78b38ddb69346
Ujval Joshi <contact@ujvaljoshi.com>
2018-10-23 08:25:40 +0530

    Updated form

--- src/pages/index.js
+++ src/pages/index.js
@@ -34,23 +34,13 @@ class IndexPage extends React.Component {
       <Layout>
         <form
           name="contact"
-          method="POST"
+          method="post"
           data-netlify="true"
           data-netlify-honeypot="bot-field"
         >
           <p>
             <label>
               Your Name: <input type="text" name="name" />
-            </label>
-          </p>
-          <p>
-            <label>
-              Your Email: <input type="email" name="email" />
-            </label>
-          </p>
-          <p>
-            <label>
-              Message: <textarea name="message" />
             </label>
           </p>
           <p>
